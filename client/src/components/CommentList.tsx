28ac6a5888931e0a683b761256b9c3882bd8a68e
nhamonin <nazarii.hamonin@gmail.com>
2024-03-10 21:13:30 +0200

    Add getContentByStatus function to CommentList component

--- client/src/components/CommentList.tsx
+++ client/src/components/CommentList.tsx
@@ -1,10 +1,30 @@
 import { IComment } from '../types';
 
 const CommentList = ({ comments }: { comments: IComment[] }) => {
+  const getContentByStatus = (comment: IComment): string => {
+    let content = '';
+
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation';
+        break;
+      case 'rejected':
+        content = 'This comment has been rejected';
+        break;
+      default:
+        break;
+    }
+
+    return content;
+  };
+
   return (
     <ul>
       {comments.map((comment) => (
-        <li key={comment.id}>{comment.content}</li>
+        <li key={comment.id}>{getContentByStatus(comment)}</li>
       ))}
     </ul>
   );
